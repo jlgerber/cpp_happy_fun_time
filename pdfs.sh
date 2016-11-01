fa6400b411b8d1b76bdb3de56516fd237e76bbf5
jonathan gerber <jlgerber@gmail.com>
2016-11-01 05:38:02 -0700

    added script to generate pdfs

--- pdfs.sh
+++ pdfs.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
 
+pandoc -f markdown_github -t latex -o pdfs/session01.pdf session_01/session01.md
+pandoc -f markdown_github -t latex -o pdfs/session02.pdf session_02/session02.md
+pandoc -f markdown_github -t latex -o pdfs/session03.pdf session_03/session03.md
+pandoc -f markdown_github -t latex -o pdfs/session04.pdf session_04/session04.md
+pandoc -f markdown_github -t latex -o pdfs/session05.pdf session_05/session05.md

d75052e2158fa730cc580a9509f47bbc272993af
Jonathan Gerber <jlgerber@gmail.com>
2017-03-21 00:38:49 -0700

    updated pdfs and ebook

--- bookit.sh
+++ bookit.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-pandoc --verbose -S -o cppForPythonistas.epub title.txt \
+pandoc  -S -o cppForPythonistas.epub title.txt \
 forward.md \
 session_01/session01.md \
 session_02/session02.md \
@@ -14,4 +14,5 @@ session_09/session09.md \
 session_10/session10.md \
 session_11/session11.md \
 session_12/session12.md \
-session_13/session13.md
+session_13/session13.md \
+session_14/session14.md
